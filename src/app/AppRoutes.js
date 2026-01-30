2d1dc1f0e19c82a0b5e9d96a5f2a828cb0edf46c
Priyanka Boosani <Priyanka.Boosani@Precisionvh.com>
2026-01-30 14:35:24 +0530

    removed interviewpanel page in masters

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -12,33 +12,23 @@ import Tokenexp from "../modules/auth/services/Tokenexp";
 import JobGradePage from "../modules/master/pages/JobGrade/JobGradePage";
 import LocationPage from "../modules/master/pages/Location/LocationPage";
 import PositionPage from "../modules/master/pages/Position/PositionPage";
-// import SpecialCategory from "../modules/master/pages/SpecialCategory/SpecialCategoryPage";
 import CategoryPage from "../modules/master/pages/Category/CategoryPage";
 import DocumentPage from "../modules/master/pages/Document/DocumentPage";
 import UserPage from "../modules/master/pages/User/UserPage";
-import Home from "../modules/master/pages/Home"; // dashboard/home
 import JobPostingsList from "../modules/jobPosting/pages/JobPostingsList";
 import CreateRequisition from "../modules/jobPosting/pages/CreateRequisition";
 import GenericOrAnnexuresPage from "../modules/master/pages/GenericOrAnnexures/GenericOrAnnexuresPage";
 import CertificationPage from "../modules/master/pages/CertificationPage/CertificationPage";
 import NonAdminRoute from "./NonAdminRoute";
 import AdminRoute from "./AdminRoute";
-import CandidatePreview from "../modules/candidatePreview/candidatePreviewPage";
-
-
 import AddPosition from "../modules/jobPosting/pages/AddPosition";
-
 // Auth & layout helpers
 import PrivateRoute from "../modules/auth/services/PrivateRoute";
 import DepartmentPage from "../modules/master/pages/Department/DepartmentPage"
-// import CategoryPage from "../modules/master/pages/Category/CategoryPage";
 import InterviewPanel from "../modules/committeeManagement/InterviewPanelPage";
-//import InterviewPanel from "../modules/master/pages/InterviewPanel";
-import SpecialCategoryPage from "../modules/master/pages/SpecialCategory/SpecialCategoryPage";
 import CandidatePreviewPage from "../modules/candidatePreview/candidatePreviewPage";
 import CandidateVerification from "../modules/Verification/CandidateVerification";
 import CandidateScreening from "../modules/candidatePreview/CandidateScreening";
-// import PositionPage from "../modules/master/pages/Position/PositionPage";
 // Lazy loaded components
 const Layout = React.lazy(() => import("../shared/components/Layout"));
 
@@ -75,20 +65,15 @@ const AppRoutes = () => {
                 <Route path="/jobgrade" element={<JobGradePage />} />
                 <Route path="/position" element={<PositionPage />} />
                 <Route path="/category" element={<CategoryPage />} />
-                <Route path="/specialcategory" element={<SpecialCategoryPage />} />
                 <Route path="/certification" element={<CertificationPage />} />
                 <Route path="/document" element={<DocumentPage />} />
-
                 <Route path="/generic-or-annexures" element={<GenericOrAnnexuresPage />} />
-              </Route>              {/* ---------- NON-ADMIN ONLY ROUTES ---------- */}
+              </Route>            
+                {/* ---------- NON-ADMIN ONLY ROUTES ---------- */}
               <Route element={<NonAdminRoute />}>
                 <Route path="/job-posting" element={<JobPostingsList />} />
                 <Route path="/job-posting/create-requisition" element={<CreateRequisition />} />
-                <Route
-                  path="/job-posting/:requisitionId/add-position"
-                  element={<AddPosition />}
-                />
-
+                <Route path="/job-posting/:requisitionId/add-position" element={<AddPosition />} />
                 <Route path="/candidate-preview" element={<CandidatePreviewPage />} />
                 <Route path="/candidate-workflow" element={<CandidateScreening />} />
                 <Route path="/candidate-verification" element={<CandidateVerification />} />
