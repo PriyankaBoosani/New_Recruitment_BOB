e41a766afc72375f429bc7aae20344f5f20c0196
Dheemanth <dheemanth.vadde@sagarsoft.in>
2026-03-25 15:53:38 +0530

    cahnges

--- src/modules/auth/pages/Login.js
+++ src/modules/auth/pages/Login.js
@@ -141,93 +141,22 @@ const Login = () => {
 
 
   return (
-    <div className="login-container">
-      <div className="left-panel">
-        <img src={pana} alt="Illustration" />
-        {/* <h2>बैंक ऑफ़ बड़ौदा</h2>
-        <h3>Bank of Baroda</h3> */}
-      </div>
-
-      <div className="right-panel">
-        <div className="logo">
-          <img src={BobLogo} alt="Logo" />
-          <h4>Recruitment Tracking System</h4>
+      <div className="login-container">
+        <div className="left-panel">
+          <img src={pana} alt="Illustration" />
         </div>
-
-        <form className="login_form" onSubmit={handleLogin}>
-          <label>Email Id:</label>
-          <input
-            type="email"
-            value={email}
-            required
-            placeholder="Enter email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <label>Password:</label>
-          <div className="" style={{ position: 'relative' }}>
-            <input
-              type={showPassword ? "text" : "password"}
-              value={password}
-              required
-              placeholder="Enter password"
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ paddingRight: '40px' }}
-            />
-            <FontAwesomeIcon
-              icon={showPassword ? faEye : faEyeSlash}
-              onClick={() => setShowPassword(!showPassword)}
-              style={{
-                position: 'absolute',
-                right: '15px',
-                top: '35%',
-                transform: 'translateY(-50%)',
-                cursor: 'pointer',
-                color: '#666',
-              }}
-              title={showPassword ? 'Hide password' : 'Show password'}
-            />
+  
+        <div className="right-panel">
+          <div className="logo">
+            <img src={BobLogo} alt="Logo" />
+            <h4>Recruitment Tracking System</h4>
           </div>
-          {unverifiedUserId && (
-            <button
-              type="button"
-              className="resend-btn my-2 mb-3"
-              style={{
-                borderRadius: "20px",
-                backgroundColor: "#fff",
-                border: "1px solid #ff6b00",
-                padding: "4px 12px",
-                color: "#ff6b00",
-              }}
-              onClick={handleResendVerification}
-            >
-              Resend Verification Email
-            </button>
-          )}
-
-
-          {/* <div className="actions">
-            <span className="forgot-link">Forgot password?</span>
-          </div> */}
-
-          <button className="login-button" type="submit">
-            LOGIN
+  
+          <button className="login-button" onClick={handleLogin}>
+            Login with Microsoft
           </button>
-
-          {/* <div className="divider">OR</div>
-
-          <button type="button" className="google-button">
-            <img src="/images/google-icon.png" alt="Google" />
-            Sign up with Google
-          </button> */}
-          <p className="register-link">
-            Forgot Password? <Link to="/forgot-password">Click here</Link>
-          </p>
-
-
-        </form>
+        </div>
       </div>
-    </div>
   );
 };
 
