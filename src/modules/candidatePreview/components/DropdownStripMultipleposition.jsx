b976507cbd49e047d144bbbec0494b502a85a7a1
saiteja.bolla <b.saiteja855@gmail.com>
2026-05-14 16:27:19 +0530

    Multipositions dropdow fixes

--- src/modules/candidatePreview/components/DropdownStripMultipleposition.jsx
+++ src/modules/candidatePreview/components/DropdownStripMultipleposition.jsx
@@ -45,18 +45,34 @@ export default function DropdownStripMultipleposition({
 const Option = (props) => {
   return (
     <components.Option {...props}>
-      <input
-        type="checkbox"
-        checked={props.isSelected}
-        onChange={() => null}
+      <div
         style={{
-          marginRight: 8,
-          accentColor: "#FFFFFF", // white
-          cursor: "pointer",
+          display: "flex",
+          alignItems: "flex-start",
+          gap: "8px",
         }}
-      />
-      
-      <label>{props.label}</label>
+      >
+        <input
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+          style={{
+            marginTop: "4px",
+            accentColor: "#FFFFFF",
+            cursor: "pointer",
+            flexShrink: 0,
+          }}
+        />
+
+        <label
+          style={{
+            margin: 0,
+            cursor: "pointer",
+          }}
+        >
+          {props.label}
+        </label>
+      </div>
     </components.Option>
   );
 };
