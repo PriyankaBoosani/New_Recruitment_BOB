0789e219d79ea0097f2db65f7421446e7efcf311
Priyanka Boosani <Priyanka.Boosani@Precisionvh.com>
2026-06-02 18:28:13 +0530

    fixed issue - revert

--- src/modules/master/pages/EducationQualification/hooks/useExperience.js
+++ src/modules/master/pages/EducationQualification/hooks/useExperience.js
@@ -247,6 +247,20 @@ export const useExperience = () => {
           ])
         );
 
+        const documentName = docMap.get(levelId);
+
+        const mapped = {
+          educationLevel: documentName || "-",
+          course: saved?.qualification?.qualificationName || "-",
+          specialization:
+            saved?.specializations?.map((s) => ({
+              name: s.specializationName,
+              id: s.specializationId,
+            })) || [],
+          educationQualificationsId:
+            saved?.qualification?.educationQualificationsId || "-",
+        };
+
         await loadData();
 
         setShowModal(false);
