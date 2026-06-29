73261e9a31f2414a5863417e43208d60c03db092
Priyanka Boosani <Priyanka.Boosani@Precisionvh.com>
2026-06-29 15:28:22 +0530

    added intermediate required in edu modal

--- src/modules/jobPosting/component/PositionForm.jsx
+++ src/modules/jobPosting/component/PositionForm.jsx
@@ -697,14 +697,34 @@ const PositionForm = ({
                 {t("addPosition:mandatory_education")}{" "}
                 <span className="text-danger">*</span>
               </Form.Label>
-              <Button
-                size="sm"
-                disabled={isViewMode}
-                onClick={() => onEducationClick("mandatory")}
-                style={{ borderRadius: "10px" }}
-              >
-                {t("addPosition:add")}
-              </Button>
+              <div className="d-flex align-items-center gap-3">
+                <Form.Check
+                  type="checkbox"
+                  className="mancheck"
+                  id="hide-school-degrees"
+                  label="Is Intermediate / ICSE(+2) / CBSE(+2) / Diploma Required"
+                  checked={formData.isIntermediateRequired || false}
+                  disabled={isViewMode}
+                  onChange={(e) => {
+                   
+
+                    handleInputChange({
+                      target: {
+                        name: "isIntermediateRequired",
+                        value: e.target.checked,
+                      },
+                    });                    
+                  }}
+                />
+                <Button
+                  size="sm"
+                  disabled={isViewMode}
+                  onClick={() => onEducationClick("mandatory")}
+                  style={{ borderRadius: "10px" }}
+                >
+                  {t("addPosition:add")}
+                </Button>
+              </div>
             </div>
             <Form.Control
               as="textarea"
